bd5dc1cdbe138c4770f3521bab1b5d5140a01070
thiago bueno garcia <thiagobuenogarcia1@gmail.com>
2024-04-17 12:35:29 -0400

    Currently making new pages and attempting to fix bug in if statement where it would automatically skip second page and head straight to third page.

--- lua_game/main.lua
+++ lua_game/main.lua
@@ -2,10 +2,13 @@ local Button = require("components.mainScreenbutton")
 
 local button
 local scaleX, scaleY
+local currentPage = 1
+local totalPages = 4
+
 
 function love.load()
     -- Load font to 32
-    love.graphics.setFont(love.graphics.newFont(50))
+    love.graphics.setFont(love.graphics.newFont(60, "mono"))
     
     -- Load image
     background = love.graphics.newImage("assets/background.jpg")
@@ -14,7 +17,15 @@ function love.load()
     end
 
     -- Create button instance
-    button = Button.new(300, 400, 200, 75, function() love.event.quit() end)
+    button = Button.new(300, 400, 200, 75, function() 
+      if currentPage == 1 then
+        currentPage = currentPage + 1
+      elseif currentPage == 2 then
+        currentPage = currentPage + 2
+      elseif currentPage == 3 then
+        love.event.quit()  
+      end 
+    end)
 end 
 
 function love.update(dt)
@@ -35,10 +46,22 @@ function love.draw()
     love.graphics.draw(background, 0, 0, 0, scaleX, scaleY)
 
     -- Draw text
-    love.graphics.print("A Knights Voyage", 175, 100)
+    -- love.graphics.print("Page " .. currentPage .. " of " .. totalPages, 175, 100)
 
-    -- Draw button
-    button:draw()
+   
+
+    -- Draw additional content for the current page
+    -- Draw text
+    if currentPage == 1 then
+        love.graphics.print("A Knights Voyage", 145, 100)
+    elseif currentPage == 2 then
+        love.graphics.print("Page 2 content", 100, 200)
+    elseif currentPage == 3 then
+        love.graphics.print("Page 3 content", 100, 200)
+    end
+
+     -- Draw button
+     button:draw()
 
     
 end
